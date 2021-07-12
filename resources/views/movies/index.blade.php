630dde9ef5655ba6add2ad26e790f39cb8ac823a
erripollo <pollone.enrico@gmail.com>
2021-07-12 17:59:06 +0200

    Display movies informations

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'Movies Page')
 
 @section('content')
-    @foreach ($movies as $movie)
 
-        <h2>{{$movie->title}}</h2>
+    <h1>Movies</h1>
+    @foreach ($movies as $movie)
+        <div class="card">
+            <h2>{{$movie->title}}</h2>
+            <h3>{{$movie->original_title}}</h3>
+            <div>
+                <span>Nationality: {{$movie->nationality}} - Date: {{$movie->date}}</span>
+            </div>
+            <h4>Vote: {{$movie->vote}}</h4>
+        </div>
         
     @endforeach
 @endsection
