d23026a2216511bd1234291d85964418f1382556
erripollo <pollone.enrico@gmail.com>
2021-07-12 20:51:20 +0200

    Bonus - add poster in movies database

--- bonus/resources/views/movies/index.blade.php
+++ bonus/resources/views/movies/index.blade.php
@@ -4,10 +4,20 @@
     
 @section('content')
 
-    @foreach ($movies as $movie)
-        
-        <h2>{{$movie->title}}</h2>
-        
-    @endforeach
+    <div class="container">
+        @foreach ($movies as $movie)
+            
+            <div class="card">
+              <img src="{{$movie->poster}}" alt="">
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Body</p>
+              </div>
+            </div>
+            <h2>{{$movie->title}}</h2>
+
+            
+        @endforeach
+    </div>
     
 @endsection
